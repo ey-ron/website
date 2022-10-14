c02586cb8d81d1b64f952d70ef4b81b018889e98
ey-ron <ey_ron@icloud.com>
2022-10-14 22:50:02 +0800

    commit

---
+++ constants/data.js
@@ -0,0 +1,119 @@
+const userData = {
+  githubUsername: "ey-ron",
+  name: "Aaron Serrano",
+  designation: "Programmer",
+  avatarUrl: "/avatar.jpg",
+  email: "aaronbserrano@yahoo.com",
+  phone: "+65 98145443",
+  address: "Singapore",
+  projects: [
+    {
+      title: "Tailwind Master Kit",
+      link: "https://tailwindmasterkit.com",
+      imgUrl: "/tmk.jpg",
+    },
+    {
+      title: "PlaceholderTech",
+      link: "https://placeholdertech.in",
+      imgUrl: "/placeholdertech.png",
+    },
+    {
+      title: "Portfolio",
+      link: "https://manuarora.in",
+      imgUrl: "/portfolio.png",
+    },
+    {
+      title: "CovidRescue",
+      link: "https://covidrescue.co.in",
+      imgUrl: "/covidrescue.png",
+    },
+    {
+      title: "Algochurn",
+      link: "https://algochurn.com",
+      imgUrl: "/algochurn.png",
+    },
+    {
+      title: "Playground",
+      link: "https://play.placeholdertech.in",
+      imgUrl: "/playground.png",
+    },
+    {
+      title: "Feedmeback",
+      link: "https://feedmeback.vercel.app",
+      imgUrl: "/feedmeback.png",
+    },
+    {
+      title: "Devmedium",
+      link: "https://devmedium.vercel.app",
+      imgUrl: "/devmedium.png",
+    },
+  ],
+  about: {
+    title:
+      "A software developer with a passion of catching up with the latest technology available, proposing solutions and ensuring the delivery of quality content",
+    description: [
+      `I've been into computer since I was a kid. I started loving the machine because of games, and suddenly realized that there are so much to do and can achieve in a computer.`,
+      `I took IT as course in College and it introduced me to another dimension. I am amazed about how cool programming is.`,
+      `I started working in Accenture as a software developer focusing in COBOL programming language. Eventually, I started to work in different teams using several technologies like Java, .NET, Unix, etc. It didn't take too long before I started to propose solutions to most problems that our instituion haven't explored, I've been working on ideas and soon after are the proof-of-concepts.`,
+      `As time goes by, I felt that something is missing. I am jealous on how technology improves like a speed of light and me not being able to understand. It opened up my heart that I am looking for more challenge and I started working on passion projects that involves modern programming languages. I started coding Python, Solidity for Blockchain Smart Contracts, ReactJS, Bootstrap, CSS, Unity as well for game development.`,
+      `I do play Airsoft as a hobby. It makes me feel super awesome, specially if you hit so many enemies :D`
+    ],
+    currentProject: "Tailwind Master Kit",
+    currentProjectUrl: "https://tailwindmasterkit.com",
+  },
+  experience: [
+    {
+      title: "Senior Consultant",
+      company: "OCBC",
+      year: "2022",
+      companyLink: "https://www.uobgroup.com/uobgroup/default.page",
+      desc: "Development and Support for SWIFT Alliance Access - Payments.",
+    },
+    {
+      title: "Senior Application Consultant",
+      company: "UOB",
+      year: "2019",
+      companyLink: "https://www.uobgroup.com/uobgroup/default.page",
+      desc: "Consultant for Trade Transformation Program.",
+    },
+    {
+      title: "Application Developer",
+      company: "UOB",
+      year: "2018",
+      companyLink: "https://www.uobgroup.com/uobgroup/default.page",
+      desc: "Development and Support for FITAS - Trade Finance.",
+    },
+    {
+      title: "Analyst Programmer",
+      company: "UOB",
+      year: "2015",
+      companyLink: "https://www.uobgroup.com/uobgroup/default.page",
+      desc: "Development and Support for FITAS - Trade Finance.",
+    },
+    {
+      title: "Software Engineer",
+      company: "Accenture",
+      year: "2012",
+      companyLink: "https://www.accenture.com/ph-en",
+      desc: "AS400 - COBOL Programmer. Works on proof-of-concepts, development and support for Caterpillar Project",
+    },
+    {
+      title: "Graduation",
+      company: "Baliuag University, Philippines.",
+      year: "2010",
+      companyLink: "https://upes.co.in",
+      desc: "Major in Information Technology.",
+    },
+  ],
+  resumeUrl:
+    "https://drive.google.com/file/d/19I88XUsrDZGH__yUyAZf7JApJX3nG07j/view?usp=sharing",
+  socialLinks: {
+    instagram: "https://instagram.com/eyron__",
+    twitter: "https://twitter.com/_zXero",
+    linkedin: "https://www.linkedin.com/in/aaronserrano/",
+    github: "https://github.com/ey-ron",
+    facebook: "https://facebook.com/aaronbserrano",
+  },
+};
+
+export default userData;
---
+++ pages/index.js
@@ -0,0 +1,37 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import ContainerBlock from "../components/ContainerBlock";
+import FavouriteProjects from "../components/FavouriteProjects";
+import LatestCode from "../components/LatestCode";
+import Hero from "../components/Hero";
+import getLatestRepos from "@lib/getLatestRepos";
+import userData from "@constants/data";
+import Experience from "@components/Experience";
+
+export default function Home({ repositories }) {
+  return (
+    <ContainerBlock
+      title="Portfolio - Aaron Serrano"
+      description="My Personal Portfolio"
+    >
+      <Hero />
+      {/*<FavouriteProjects />*/}
+      <Experience/>
+      {/*<LatestCode repositories={repositories} />*/}
+    </ContainerBlock>
+  );
+}
+
+export const getServerSideProps = async () => {
+  console.log(process.env.GITHUB_AUTH_TOKEN);
+  let token = process.env.GITHUB_AUTH_TOKEN;
+
+  const repositories = await getLatestRepos(userData, token);
+  // console.log("REPOSITORIES", repositories);
+
+  return {
+    props: {
+      repositories,
+    },
+  };
+};
