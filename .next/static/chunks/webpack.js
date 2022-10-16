b28b9c363e65894e6515bc90600ea16f6e1036ab
ey-ron <ey_ron@icloud.com>
2022-10-16 12:59:27 +0800

    updated

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c5ecce137f6dee998865"; }
+/******/ 		__webpack_require__.h = function() { return "f031b9fd1abdc4f407b5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,7 @@ module.exports = {
       },
       screens: {
         '2sm': '310px',
+        'sml': '560px',
         'sm': '640px',
         'md': '768px',
         'lg': '1024px',
